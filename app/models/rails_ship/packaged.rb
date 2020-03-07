9173b22263daa973bb3f301f64fcc1e39d314b53
qinmingyuan <mingyuan0715@foxmail.com>
2020-03-08 02:23:47 +0800

    cached trade item status

--- app/models/rails_ship/packaged.rb
+++ app/models/rails_ship/packaged.rb
@@ -2,11 +2,18 @@ module RailsShip::Packaged
   extend ActiveSupport::Concern
 
   included do
+    attribute :trade_item_status, :string
+
     belongs_to :package, inverse_of: :packageds
     belongs_to :trade_item
 
+    before_save :sync_trade_item_status
     after_create :update_status
     after_destroy :revert_status
+  end
+
+  def sync_trade_item_status
+    self.trade_item_status = trade_item.status
   end
 
   def update_status
@@ -14,7 +21,7 @@ module RailsShip::Packaged
   end
 
   def revert_status
-    self.trade_item.update status: 'paid'
+    self.trade_item.update status: self.trade_item_status
   end
 
 end
