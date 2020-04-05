bf8346fc4f8810e28d3f877b22dd51559371037d
qinmingyuan <mingyuan0715@foxmail.com>
2020-04-05 18:27:43 +0800

    make test work

--- test/controllers/ship/admin/packages_controller_test.rb
+++ test/controllers/ship/admin/packages_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 class Ship::Admin::PackagesControllerTest < ActionDispatch::IntegrationTest
 
   setup do
-    @ship_admin_package = create ship_admin_packages
+    @package = create :packages
   end
 
   test 'index ok' do
@@ -17,30 +17,30 @@ class Ship::Admin::PackagesControllerTest < ActionDispatch::IntegrationTest
 
   test 'create ok' do
     assert_difference('Package.count') do
-      post admin_packages_url, params: { #{singular_table_name}: { #{attributes_string} } }
+      post admin_packages_url, params: {}
     end
 
     assert_response :success
   end
 
   test 'show ok' do
-    get admin_package_url(@ship_admin_package)
+    get admin_package_url(@package)
     assert_response :success
   end
 
   test 'edit ok' do
-    get edit_admin_package_url(@ship_admin_package)
+    get edit_admin_package_url(@package)
     assert_response :success
   end
 
   test 'update ok' do
-    patch admin_package_url(@ship_admin_package), params: { #{singular_table_name}: { #{attributes_string} } }
+    patch admin_package_url(@package), params: {  }
     assert_response :success
   end
 
   test 'destroy ok' do
     assert_difference('Package.count', -1) do
-      delete admin_package_url(@ship_admin_package)
+      delete admin_package_url(@package)
     end
 
     assert_response :success
