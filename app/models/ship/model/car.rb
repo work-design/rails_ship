e70106b5543c24bb4a89be693af10b723fa959a5
MingyuanQin <mingyuan0715@foxmail.com>
2021-03-31 20:13:25 +0800

    store

--- app/models/ship/model/car.rb
+++ app/models/ship/model/car.rb
@@ -5,10 +5,15 @@ module Ship
     included do
       attribute :location, :string
       attribute :number, :string
+      attribute :detail, :json
 
       belongs_to :user, class_name: 'Auth::User'
 
       has_one_attached :registration
+    end
+
+    def ocr
+      TencentHelper.registration_ocr(registration.url)
     end
 
   end
--- app/models/ship/model/driver.rb
+++ app/models/ship/model/driver.rb
@@ -5,10 +5,15 @@ module Ship
     included do
       attribute :name, :string
       attribute :number, :string
+      attribute :detail, :json
 
       belongs_to :user, class_name: 'Auth::User'
 
       has_one_attached :license
+    end
+
+    def ocr
+      TencentHelper.license_ocr(license.url)
     end
 
   end
