4c11514b50a01c4b4ca214299c2157603ee81aac
MingyuanQin <mingyuan0715@foxmail.com>
2021-04-01 21:15:39 +0800

    update

--- app/models/ship/model/driver.rb
+++ app/models/ship/model/driver.rb
@@ -10,10 +10,21 @@ module Ship
       belongs_to :user, class_name: 'Auth::User'
 
       has_one_attached :license
+
+      after_create_commit :ocr_later
+    end
+
+    def ocr_later
+      DriverOcrJob.perform_later(self)
+      for_update
     end
 
     def ocr
       TencentHelper.license_ocr(license.url)
+    end
+
+    def for_update
+      broadcast_action_to 'car_new', action: :update, target: 'car_update', partial: 'ship/my/drivers/edit_form', locals: { driver: self }
     end
 
   end
