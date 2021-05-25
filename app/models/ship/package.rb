20700c134b565cbc9e14ea8b2061ffd9aed30109
qinmingyuan <mingyuan0715@foxmail.com>
2021-05-25 22:27:10 +0800

    rename module

--- app/models/ship/package.rb
+++ app/models/ship/package.rb
@@ -1,6 +1,6 @@
 module Ship
   class Package < ApplicationRecord
     include Model::Package
-    include Wait::Model::Waiting if defined? RailsWait
+    include Wait::Ext::Waiting if defined? RailsWait
   end
 end
