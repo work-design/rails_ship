a8333b4c5ae6e40aff1f2f2d8524618d33f9c9ee
qinmingyuan <mingyuan0715@foxmail.com>
2022-06-15 15:51:06 +0800

    fix err

--- app/controllers/ship/in/boxes_controller.rb
+++ app/controllers/ship/in/boxes_controller.rb
@@ -8,7 +8,7 @@ module Ship
       }
       q_params.merge! params.permit(:box_specification_id)
 
-      @boxes = Box.includes(:box_specification, :trade_item).default_where(q_params).page(params[:page])
+      @boxes = Box.includes(:box_specification, :trade_item).ordered.default_where(q_params).page(params[:page])
     end
 
     def invest
@@ -17,7 +17,7 @@ module Ship
       }
       q_params.merge! params.permit(:box_specification_id)
 
-      @boxes = Box.includes(:box_specification, :trade_item).default_where(q_params).page(params[:page])
+      @boxes = Box.includes(:box_specification, :trade_item).ordered.default_where(q_params).page(params[:page])
     end
 
 
@@ -28,7 +28,7 @@ module Ship
       }
       q_params.merge! params.permit(:box_specification_id)
 
-      @boxes = Box.includes(:box_specification, :trade_item).default_where(q_params).page(params[:page])
+      @boxes = Box.includes(:box_specification, :trade_item).ordered.default_where(q_params).page(params[:page])
     end
 
     private
