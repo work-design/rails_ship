ad82620962878becefdf30f45dae7bc8c302e79d
qinmingyuan <mingyuan0715@foxmail.com>
2022-06-16 17:39:39 +0800

    use the corrent cart

--- app/controllers/ship/box_specifications_controller.rb
+++ app/controllers/ship/box_specifications_controller.rb
@@ -1,7 +1,8 @@
 module Ship
   class BoxSpecificationsController < BaseController
     before_action :set_box_specification, only: [:show]
-    before_action :set_cart, only: [:index, :rent]
+    before_action :set_use_cart, only: [:index]
+    before_action :set_rent_cart, only: [:rent]
 
     def index
       @box_specifications = BoxSpecification.page(params[:page])
@@ -16,8 +17,12 @@ module Ship
       @box_specification = BoxSpecification.find params[:id]
     end
 
-    def set_cart
-      @cart = current_carts.find_or_create_by(good_type: 'Ship::BoxSpecification')
+    def set_use_cart
+      @cart = current_carts.find_or_create_by(good_type: 'Ship::BoxSpecification', aim: 'use')
+    end
+
+    def set_rent_cart
+      @cart = current_carts.find_or_create_by(good_type: 'Ship::BoxSpecification', aim: 'rent')
     end
 
   end
