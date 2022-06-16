70d127044bfc9eafd40bf9f2c3efe3fd78d19c8f
qinmingyuan <mingyuan0715@foxmail.com>
2022-06-16 17:27:55 +0800

    not use current cart

--- app/controllers/ship/box_specifications_controller.rb
+++ app/controllers/ship/box_specifications_controller.rb
@@ -1,6 +1,7 @@
 module Ship
   class BoxSpecificationsController < BaseController
     before_action :set_box_specification, only: [:show]
+    before_action :set_cart, only: [:index, :rent]
 
     def index
       @box_specifications = BoxSpecification.page(params[:page])
@@ -13,6 +14,10 @@ module Ship
     private
     def set_box_specification
       @box_specification = BoxSpecification.find params[:id]
+    end
+
+    def set_cart
+      @cart = current_carts.find_or_create_by(good_type: 'Ship::BoxSpecification')
     end
 
   end
