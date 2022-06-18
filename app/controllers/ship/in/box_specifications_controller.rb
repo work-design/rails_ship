21bd3210bb893a8deeb47966c9c8f2d9ccc5098d
qinmingyuan <mingyuan0715@foxmail.com>
2022-06-18 14:38:10 +0800

    carts

--- app/controllers/ship/in/box_specifications_controller.rb
+++ app/controllers/ship/in/box_specifications_controller.rb
@@ -1,5 +1,7 @@
 module Ship
   class In::BoxSpecificationsController < In::BaseController
+    before_action :set_rent_cart, only: [:rent]
+    before_action :set_use_cart, only: [:buy]
 
     def index
       @box_specifications = BoxSpecification.page(params[:page])
@@ -11,6 +13,15 @@ module Ship
 
     def buy
       @box_specifications = BoxSpecification.page(params[:page])
+    end
+
+    private
+    def set_rent_cart
+      @cart = current_organ.organ_carts.find_or_create_by(good_type: 'Ship::BoxSpecification', aim: 'rent')
+    end
+
+    def set_use_cart
+      @cart = current_organ.organ_carts.find_or_create_by(good_type: 'Ship::BoxSpecification', aim: 'use')
     end
 
   end
