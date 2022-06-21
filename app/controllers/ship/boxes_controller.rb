f8139fa082d1812fbe6da32a13122b7c50bc379a
qinmingyuan <mingyuan0715@foxmail.com>
2022-06-21 09:40:37 +0800

    use redirect

--- app/controllers/ship/boxes_controller.rb
+++ app/controllers/ship/boxes_controller.rb
@@ -1,5 +1,6 @@
 module Ship
   class BoxesController < BaseController
+    before_action :set_box, only: [:qrcode]
 
     def qrcode
       redirect_to controller: 'ship/me/boxes', action: 'qrcode', id: params[:id], host: @box.organ.host
