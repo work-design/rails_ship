2ae87cf480503a97e60d2d8a4ecdf5d84720dfb4
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-14 14:25:34 +0800

    one can maintain from station for pacage

--- app/controllers/ship/admin/trade_items_controller.rb
+++ app/controllers/ship/admin/trade_items_controller.rb
@@ -2,6 +2,7 @@ module Ship
   class Admin::TradeItemsController < Admin::BaseController
     before_action :set_address, only: [:packable, :package, :packaged]
     before_action :set_trade_item, only: [:show, :edit, :update, :destroy]
+    before_action :set_stations, only: [:edit, :update]
 
     def index
       q_params = {
@@ -48,11 +49,14 @@ module Ship
       @trade_item = Trade::TradeItem.find(params[:id])
     end
 
+    def set_stations
+      @stations = Station.default_where(default_params)
+    end
+
     def trade_item_params
       params.fetch(:trade_item, {}).permit(
-        :good_id,
-        :number,
-        :status
+        :from_station_id,
+        :station_id
       )
     end
 
