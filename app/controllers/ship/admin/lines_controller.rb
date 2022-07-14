ecd2b6cfb16e0980585aaa9380c44e4f99c5e8b3
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-14 13:55:54 +0800

    filter line by station organ id

--- app/controllers/ship/admin/lines_controller.rb
+++ app/controllers/ship/admin/lines_controller.rb
@@ -1,5 +1,13 @@
 module Ship
   class Admin::LinesController < Admin::BaseController
 
+    def index
+      q_params = {
+        'line_stations.organ_id': current_organ.id
+      }
+
+      @lines = Line.default_where(q_params).page(params[:page])
+    end
+
   end
 end
--- app/models/ship/model/line_station.rb
+++ app/models/ship/model/line_station.rb
@@ -5,12 +5,19 @@ module Ship
     included do
       attribute :position, :integer
 
+      belongs_to :organ, class_name: 'Org::Organ', optional: true
+
       belongs_to :line, counter_cache: true
       belongs_to :station
 
       acts_as_list scope: [:line_id]
 
+      before_validation :sync_organ, if: -> { station_id_changed? }
       after_create_commit :sync_names_to_line
+    end
+
+    def sync_organ
+      self.organ_id = station.organ_id
     end
 
     def sync_names_to_line
