e30b2d5c92c8683633cc4c907e055f064c5e80e5
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-19 16:21:38 +0800

    methods direct package trade items

--- app/models/ship/ext/order.rb
+++ app/models/ship/ext/order.rb
@@ -6,5 +6,18 @@ module Ship
       has_many :packages, -> { distinct }, through: :trade_items
     end
 
+    def package
+      pack = address.packages.build(default_form_params)
+      produce_plan_ids = trade_items.deliverable.pluck(:produce_plan_id).uniq
+
+      pack.user_id = user_id
+      pack.produce_plan_id = produce_plan_ids[0]
+      trade_items.each do |trade_item|
+        p = trade_item.packageds.build
+        p.package = pack
+        p.save
+      end
+    end
+
   end
 end
