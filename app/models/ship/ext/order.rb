3956f280e6c53137a8590c70dbae23f8de2504ae
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-19 16:33:45 +0800

    fix direct package

--- app/models/ship/ext/order.rb
+++ app/models/ship/ext/order.rb
@@ -7,11 +7,8 @@ module Ship
     end
 
     def package
-      pack = address.packages.build(default_form_params)
-      produce_plan_ids = trade_items.deliverable.pluck(:produce_plan_id).uniq
-
+      pack = address.packages.build(organ_id: organ_id)
       pack.user_id = user_id
-      pack.produce_plan_id = produce_plan_ids[0]
       trade_items.each do |trade_item|
         p = trade_item.packageds.build
         p.package = pack
