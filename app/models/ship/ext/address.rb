9a065ce94875bbd36ce0751c43bda7026c05fa5e
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-21 16:51:40 +0800

    sync station id to packages

--- app/models/ship/ext/address.rb
+++ app/models/ship/ext/address.rb
@@ -12,10 +12,15 @@ module Ship
       has_many :shipments, as: :shipping
 
       before_validation :sync_area, if: -> { station_id_changed? && station.present? }
+      after_save :sync_station_id, if: -> { station_id.present? && saved_change_to_station_id? }
     end
 
     def sync_area
       self.area_id = station.area_id
+    end
+
+    def sync_station_id
+      packages.where(station_id: nil).update_all(station_id: station_id)
     end
 
   end
