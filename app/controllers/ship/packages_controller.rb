6d8b8c85f87f3d525bee26778e16aa34063e16dd
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-28 10:36:39 +0800

    if user has right to pacakges

--- app/controllers/ship/packages_controller.rb
+++ app/controllers/ship/packages_controller.rb
@@ -1,12 +1,15 @@
 module Ship
   class PackagesController < BaseController
+    before_action :require_login
     before_action :set_package, :set_member, only: [:qrcode]
 
     def qrcode
       if @member
         redirect_to({ controller: 'ship/me/packages', action: 'qrcode', id: params[:id], host: @package.organ.host }, allow_other_host: true)
-      else
+      elsif current_user && @package.user == current_user
         redirect_to({ controller: 'ship/my/packages', action: 'qrcode', id: params[:id], host: @package.organ.host }, allow_other_host: true)
+      else
+        render 'qrcode'
       end
     end
 
