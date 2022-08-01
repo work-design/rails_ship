c4bad1d5b6281bc96c6af53c2ba9136282ccf05a
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-01 16:09:12 +0800

    gemfile

--- app/models/ship/model/shipment_log.rb
+++ app/models/ship/model/shipment_log.rb
@@ -4,12 +4,16 @@ module Ship
 
     included do
       attribute :expected_leave_at, :datetime
-      attribute :expected_arrive_at, :datetime
       attribute :left_at, :datetime
       attribute :arrived_at, :datetime
+      attribute :prepared_at, :datetime
 
       belongs_to :shipment
       belongs_to :line_station
+    end
+
+    def expected_arrive_at
+      (arrived_at || expected_leave_at) + shipment.expected_minutes
     end
 
   end
