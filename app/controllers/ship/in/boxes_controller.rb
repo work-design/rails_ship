0746860aff6e7ff5ba1fdeb8d1d8f384cfa8522a
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-11 16:45:51 +0800

    rename

--- app/controllers/ship/in/boxes_controller.rb
+++ app/controllers/ship/in/boxes_controller.rb
@@ -8,7 +8,7 @@ module Ship
       }
       q_params.merge! params.permit(:box_specification_id)
 
-      @boxes = Box.includes(:trade_item, :using_box_logs).ordered.order(trade_item_id: :desc).default_where(q_params).page(params[:page])
+      @boxes = Box.includes(:item, :using_box_logs).ordered.order(item_id: :desc).default_where(q_params).page(params[:page])
     end
 
     def invest
@@ -17,7 +17,7 @@ module Ship
       }
       q_params.merge! params.permit(:box_specification_id)
 
-      @boxes = Box.includes(:box_specification, :trade_item).ordered.default_where(q_params).page(params[:page])
+      @boxes = Box.includes(:box_specification, :item).ordered.default_where(q_params).page(params[:page])
     end
 
 
@@ -28,7 +28,7 @@ module Ship
       }
       q_params.merge! params.permit(:box_specification_id)
 
-      @boxes = Box.includes(:box_specification, :trade_item).ordered.default_where(q_params).page(params[:page])
+      @boxes = Box.includes(:box_specification, :item).ordered.default_where(q_params).page(params[:page])
     end
 
     private
