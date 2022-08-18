443e7fc0a3c18c3a3f03df86b9e92980fa29aaa6
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-18 17:36:18 +0800

    show

--- app/models/ship/ext/organ.rb
+++ app/models/ship/ext/organ.rb
@@ -4,6 +4,7 @@ module Ship
 
     included do
       has_many :boxes, class_name: 'Ship::Box', foreign_key: :held_organ_id
+      has_many :box_hosts, class_name: 'Ship::BoxHost'
       has_many :stations, class_name: 'Ship::Station'
     end
 
