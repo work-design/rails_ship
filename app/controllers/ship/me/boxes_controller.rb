1b15f5ba583872759235720dde83cca35a8fcf42
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-26 15:48:34 +0800

    suuport boxed type

--- app/controllers/ship/me/boxes_controller.rb
+++ app/controllers/ship/me/boxes_controller.rb
@@ -1,7 +1,7 @@
 module Ship
   class Me::BoxesController < Me::BaseController
     before_action :set_box, only: [:qrcode, :in, :out, :loaded, :unloaded]
-    before_action :set_package_from_scan, only: [:in, :out]
+    before_action :set_boxed_from_scan, only: [:in, :out]
     before_action :set_shipment_from_scan, only: [:loaded, :unloaded]
 
     def index
@@ -13,8 +13,8 @@ module Ship
 
     # 装入包裹
     def in
-      if @package
-        bl = @box.box_logs.find_or_initialize_by(boxed_type: 'Ship::Package', boxed_id: @package.id)
+      if @boxed
+        bl = @box.box_logs.find_or_initialize_by(boxed_type: @boxed.class_name, boxed_id: @boxed.id)
         bl.boxed_in_at ||= Time.current
         bl.save
       end
@@ -22,8 +22,8 @@ module Ship
 
     # 取出包裹
     def out
-      if @package
-        bl = @box.box_logs.find_by(boxed_type: 'Ship::Package', boxed_id: @package.id)
+      if @boxed
+        bl = @box.box_logs.find_by(boxed_type: @boxed.class_name, boxed_id: @boxed.id)
         bl.boxed_out_at = Time.current
         bl.save
       end
@@ -55,11 +55,15 @@ module Ship
       @box = Box.default_where(default_params).find params[:id]
     end
 
-    def set_package_from_scan
+    def set_boxed_from_scan
       r = params[:result].scan(RegexpUtil.more_between('packages/', '/qrcode'))
       if r.present?
-        @package = Package.find r[0]
+        @boxed = Package.find r[0]
+        return
       end
+
+      r = params[:result].scan(RegexpUtil.more_between('production_items/', '/qrcode'))
+      @boxed = Factory::ProductionItem.find r[0] if r.present?
     end
 
     def set_shipment_from_scan
