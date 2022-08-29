db8d16dfeec40d82690e5ded7e2521da98e79909
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-29 12:31:39 +0800

    link to production item

--- app/models/ship/model/packaged.rb
+++ app/models/ship/model/packaged.rb
@@ -9,9 +9,14 @@ module Ship
       belongs_to :production_item, class_name: 'Factory::ProductionItem', optional: true
       belongs_to :package, inverse_of: :packageds, counter_cache: true
 
+      before_validation :compute_trade_item, if: -> { production_item_id.present? }
       after_create :update_status
       after_save :sync_trade_item_status, if: -> { saved_change_to_trade_item_id? }
       after_destroy :revert_status
+    end
+
+    def compute_trade_item
+      self.trade_item = package.items.packable.find_by(good_type: 'Factory::Production', good_id: production_item.production_id)
     end
 
     def update_status
