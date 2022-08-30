df6a7ef0d3422abf53613c09c8b99922e6f69de5
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-30 18:45:50 +0800

    packaged item can be any class

--- app/models/ship/model/packaged.rb
+++ app/models/ship/model/packaged.rb
@@ -9,7 +9,7 @@ module Ship
       belongs_to :good_item, optional: true, polymorphic: true
       belongs_to :package, inverse_of: :packageds, counter_cache: true
 
-      before_validation :compute_trade_item, if: -> { good_id.present? }
+      before_validation :compute_trade_item, if: -> { good_item_id.present? }
       after_create :update_status
       after_save :sync_trade_item_status, if: -> { saved_change_to_trade_item_id? }
       after_destroy :revert_status
@@ -17,7 +17,9 @@ module Ship
 
     def compute_trade_item
       if good_item_type == 'Factory::ProductionItem'
-        self.trade_item = package.address.items.packable.find_by(good_type: 'Factory::Production', good_id: good_item.production_id)
+        self.trade_item = (package.address || package.user).items.packable.find_by(good_type: 'Factory::Production', good_id: good_item.production_id)
+      elsif good_item_type == 'Ship::Box'
+        self.trade_item = (package.address || package.user).items.packable.find_by(good_type: 'Factory::BoxHost', good_id: good_item.box_host.id)
       end
     end
 
