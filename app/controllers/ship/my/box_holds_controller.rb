dff9558091e0c584d2df6a8e9ac2ef57e55dbb7a
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-31 17:15:51 +0800

    hold boxes

--- app/controllers/ship/my/box_holds_controller.rb
+++ app/controllers/ship/my/box_holds_controller.rb
@@ -2,9 +2,9 @@ module Ship
   class My::BoxHoldsController < My::BaseController
 
     def index
-      q_params = { user_id: current_user.id }
+      q_params = { user_id: current_user.id, organ_id: nil }
 
-      @box_holds = BoxHold.default_where(q_params)
+      @box_holds = BoxHold.where(q_params)
     end
 
   end
