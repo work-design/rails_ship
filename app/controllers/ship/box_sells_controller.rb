855ab45d99d16a6e6c8e7221b5809d5d2d6c8454
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-02 16:35:54 +0800

    saleable count

--- app/controllers/ship/box_sells_controller.rb
+++ app/controllers/ship/box_sells_controller.rb
@@ -3,7 +3,7 @@ module Ship
     before_action :set_box_host
 
     def index
-      @box_sells = @box_host.box_sells
+      @box_sells = @box_host.box_sells.order(price: :asc)
     end
 
     private
