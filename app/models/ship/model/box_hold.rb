e65a5e2ca20ab87b53715bbac74846873043860d
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-05 11:12:27 +0800

    buye boxes

--- app/models/ship/model/box_hold.rb
+++ app/models/ship/model/box_hold.rb
@@ -15,8 +15,9 @@ module Ship
       belongs_to :user, class_name: 'Auth::User'
       belongs_to :member, class_name: 'Org::Member', optional: true
       belongs_to :member_organ, class_name: 'Org::Organ', optional: true
-      belongs_to :box_sell, ->(o){ where(organ_id: o.organ_id, box_specification_id: o.box_specification_id) }, foreign_key: :sale_price, primary_key: :price, optional: true
-      belongs_to :box_buy, ->(o){ where(organ_id: o.organ_id, box_specification_id: o.box_specification_id) }, foreign_key: :buy_price, primary_key: :price, optional: true
+      belongs_to :box_host, ->(o) { where(organ_id: o.organ_id) }, foreign_key: :box_specification_id, primary_key: :box_specification_id, optional: true
+      belongs_to :box_sell, ->(o){ where(organ_id: o.organ_id, price: o.sell_price) }, foreign_key: :box_specification_id, primary_key: :box_specification_id, optional: true
+      belongs_to :box_buy, ->(o){ where(organ_id: o.organ_id, price: o.buy_price) }, foreign_key: :box_specification_id, primary_key: :box_specification_id, optional: true
 
       has_many :boxes, ->(o){ where(organ_id: o.organ_id, held_user_id: o.user_id) }, primary_key: :box_specification_id, foreign_key: :box_specification_id
 
