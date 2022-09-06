6d8e0a7e34d99a24f233f9acc07430f725ea49b1
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-06 14:47:34 +0800

    trigger buy

--- app/models/ship/model/box_entrust/box_buy.rb
+++ app/models/ship/model/box_entrust/box_buy.rb
@@ -4,14 +4,23 @@ module Ship
 
     included do
       belongs_to :box_proxy_buy, ->(o){ where(organ_id: o.organ_id, price: o.price) }, foreign_key: :box_specification_id, primary_key: :box_specification_id, optional: true
+      belongs_to :box_proxy_sell, ->(o){ where(organ_id: o.organ_id, price: o.price) }, foreign_key: :box_specification_id, primary_key: :box_specification_id, optional: true
 
       has_many :box_holds, ->(o){ where(organ_id: o.organ_id, buy_price: o.price) }, primary_key: :box_specification_id, foreign_key: :box_specification_id
+      has_many :box_sells, ->(o) { default_where(organ_id: o.organ_id, 'price-lte': o.price) }, primary_key: :box_specification_id, foreign_key: :box_specification_id
 
       before_validation :init_box_proxy_buy, if: -> { price.present? && (['price', 'amount'] & changes.keys).present? }
     end
 
     def init_box_proxy_buy
       box_proxy_buy || build_box_proxy_buy
+      box_proxy_sell || build_box_proxy_sell
+    end
+
+    def trigger_buy
+      o = user.orders.build
+      o.items.build(good_type: box_proxy_sell.class_name, good_id: box_proxy_sell.id, number: amount)
+      o.save
     end
 
   end
--- app/models/ship/model/box_proxy_buy.rb
+++ app/models/ship/model/box_proxy_buy.rb
@@ -9,7 +9,7 @@ module Ship
       belongs_to :box_specification
       belongs_to :organ, class_name: 'Org::Organ', optional: true
 
-      has_many :box_sells, ->(o) { where(organ_id: o.organ_id) }, primary_key: :box_specification_id, foreign_key: :box_specification_id
+      has_many :box_sells, ->(o) { default_where(organ_id: o.organ_id, 'price-lte': o.price) }, primary_key: :box_specification_id, foreign_key: :box_specification_id
     end
 
   end
