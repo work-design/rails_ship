7c8ef1fba49b603226cbb03cdce0e0affac6fe76
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-06 18:03:18 +0800

    compute reset amount

--- app/models/ship/model/box_sale/box_proxy_sell.rb
+++ app/models/ship/model/box_sale/box_proxy_sell.rb
@@ -5,16 +5,17 @@ module Ship
     included do
       attribute :sellable_count, :integer, default: 0
 
-      has_many :box_sells, ->(o) { where(organ_id: o.organ_id).order(id: :asc) }, primary_key: :box_specification_id, foreign_key: :box_specification_id
+      has_many :box_sells, ->(o) { default_where(organ_id: o.organ_id, 'rest-amount-gt': 0).order(id: :asc) }, primary_key: :box_specification_id, foreign_key: :box_specification_id
     end
 
     def order_paid(item)
-
-    end
-
-    def xx(number)
       r = box_sells.pluck(:id, :rest_amount)
       last = r.find_until(number)[-1]
+      box_sells.default_where('id-lte': last[0]).each do |box_sell|
+        box_sell.done_amount = box_sell.amount
+        box_sell.item = item
+        box_sell.save
+      end
     end
 
   end
--- app/models/ship/model/box_sell.rb
+++ app/models/ship/model/box_sell.rb
@@ -15,13 +15,15 @@ module Ship
       belongs_to :member, class_name: 'Org::Member', optional: true
       belongs_to :member_organ, class_name: 'Org::Organ', optional: true
 
+      belongs_to :item, class_name: 'Trade::Item', optional: true
+
       belongs_to :box_proxy_sell, ->(o){ where(organ_id: o.organ_id, price: o.price) }, foreign_key: :box_specification_id, primary_key: :box_specification_id, optional: true
       belongs_to :box_hold, ->(o){ where(organ_id: o.organ_id, box_specification_id: o.box_specification_id) }, primary_key: :user_id, foreign_key: :user_id
 
       has_many :items, ->(o) { where(organ_id: o.organ_id, good_type: 'Ship::BoxSale', good_id: o.box_proxy_sell&.id, member_id: o.member_id) }, class_name: 'Trade::Item', primary_key: :user_id, foreign_key: :user_id
 
       before_validation :init_box_proxy_sell, if: -> { price.present? && (['price', 'amount'] & changes.keys).present? }
-
+      before_validation :compute_rest_amount, if: -> { (['amount', 'done_amount'] & changes.keys).present? }
     end
 
     def init_box_hold
@@ -30,6 +32,10 @@ module Ship
 
     def init_box_proxy_sell
       box_proxy_sell || build_box_proxy_sell
+    end
+
+    def compute_rest_amount
+      self.rest_amount = self.amount - self.done_amount
     end
 
   end
