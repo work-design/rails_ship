05099868220b5d57aa6d63e28ecdf83e606b52c0
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-06 14:24:30 +0800

    held price

--- app/controllers/ship/my/box_entrusts_controller.rb
+++ app/controllers/ship/my/box_entrusts_controller.rb
@@ -9,14 +9,14 @@ module Ship
       q_options = { box_specification_id: @box_host.box_specification_id }
       q_options.merge! default_params
 
-      @box_hold = current_user.box_holds.order(sell_price: :asc).find_or_initialize_by(q_options)
+      @box_hold = current_user.box_holds.find_or_initialize_by(q_options)
     end
 
     def sell
       q_options = { box_specification_id: @box_host.box_specification_id }
       q_options.merge! default_params
 
-      @box_hold = current_user.box_holds.order(buy_price: :desc).find_or_initialize_by(q_options)
+      @box_hold = current_user.box_holds.find_or_initialize_by(q_options)
     end
 
     private
@@ -36,6 +36,7 @@ module Ship
     def set_overview
       @box_proxy_sells = @box_host.box_proxy_sells.order(price: :asc).limit(3)
       @box_proxy_buys = @box_host.box_proxy_buys.order(price: :desc).limit(3)
+      @box_entrusts = @box_hold.box_entrusts.order(id: :desc).limit(5)
     end
 
   end
