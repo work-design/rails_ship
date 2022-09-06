bcaa725b649afb80e491bf93da56db6a121895b1
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-06 18:26:24 +0800

    sep logic

--- app/models/ship/model/box_sale/box_proxy_sell.rb
+++ app/models/ship/model/box_sale/box_proxy_sell.rb
@@ -8,14 +8,26 @@ module Ship
       has_many :box_sells, ->(o) { default_where(organ_id: o.organ_id, 'rest-amount-gt': 0).order(id: :asc) }, primary_key: :box_specification_id, foreign_key: :box_specification_id
     end
 
+    # todo 针对交易量过大时候的优化
     def order_paid(item)
       r = box_sells.pluck(:id, :rest_amount)
-      last = r.find_until(number)[-1]
-      r = box_sells.default_where('id-lte': last[0]).map do |box_sell|
-        box_sell.done_amount = box_sell.amount
+      usable = r.find_until(item.number)
+      b_sells = box_sells.default_where('id-lte': usable[-1][0])
+      r = b_sells[0..-2].map do |box_sell|
         box_sell.item = item
+        box_sell.done_amount = box_sell.amount
         item.done_number += done_amount
+        box_sell
       end
+
+      last_sell = b_sells[-1]
+      last_sell.item = item
+      if item.done_number + last_sell.amount > item.number
+        last_sell.done_amount = item.number - item.done_number
+      else
+        last_sell
+      end
+      r << last_sell
 
       self.class.transaction do
         r.map(&:save)
