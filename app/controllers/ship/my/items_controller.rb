d3e132855993974a4431dd421820772083cdb22c
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-07 17:01:09 +0800

    erb for items

---
+++ app/controllers/ship/my/items_controller.rb
@@ -0,0 +1,16 @@
+module Ship
+  class My::ItemsController < My::BaseController
+    before_action :set_box_hold
+    before_action :set_item, only: [:show]
+
+    private
+    def set_box_hold
+      @box_hold = BoxHold.find params[:box_hold_id]
+    end
+
+    def set_item
+      @item = @box_hold.items.find params[:id]
+    end
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -257,6 +257,7 @@ Rails.application.routes.draw do
             get :buy
           end
           resources :orders, only: [:create]
+          resources :items, only: [:index, :show]
           resources :box_sells, only: [:index, :show, :create]
         end
         resources :box_hosts, only: [] do
