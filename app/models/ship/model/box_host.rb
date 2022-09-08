7e2f5a0c93d17cee78962069a1c3b635a5e9ba01
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-08 10:41:27 +0800

    box host

--- app/models/ship/model/box_host.rb
+++ app/models/ship/model/box_host.rb
@@ -1,9 +1,8 @@
 module Ship
-  module Model::BoxSale
+  module Model::BoxHost
     extend ActiveSupport::Concern
 
     included do
-      attribute :type, :string
       attribute :boxes_count, :integer, default: 0
       attribute :rented_count, :integer, default: 0
       attribute :rentable_count, :integer, default: 0
