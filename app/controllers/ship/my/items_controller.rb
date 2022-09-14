1bc4348f884cda48c86214d35e5bdaf0caf906b7
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-14 15:33:17 +0800

    fix show box sells

--- app/controllers/ship/my/items_controller.rb
+++ app/controllers/ship/my/items_controller.rb
@@ -3,6 +3,10 @@ module Ship
     before_action :set_box_hold
     before_action :set_item, only: [:show]
 
+    def index
+      @items = @box_hold.box_host.items.where(user_id: @box_hold.user_id).page(params[:page])
+    end
+
     private
     def set_box_hold
       @box_hold = BoxHold.find params[:box_hold_id]
