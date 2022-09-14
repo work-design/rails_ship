796a3aea1f79a64645be0e0e84a8f3f9bb961b2c
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-14 15:44:13 +0800

    inti box proxy buy

--- app/models/ship/ext/item.rb
+++ app/models/ship/ext/item.rb
@@ -6,14 +6,21 @@ module Ship
       has_many :packageds, class_name: 'Ship::Packaged', foreign_key: :trade_item_id, dependent: :destroy_async
       has_many :packages, class_name: 'Ship::Package', through: :packageds
 
-      belongs_to :box_hold, ->(o){ where(organ_id: o.organ_id, member_id: o.member_id, box_specification_id: o.good.box_specification_id) }, class_name: 'Ship::BoxHold', foreign_key: :user_id, primary_key: :user_id, optional: true
+      belongs_to :box_hold, ->(o) { where(organ_id: o.organ_id, member_id: o.member_id, box_specification_id: o.good.box_specification_id) }, class_name: 'Ship::BoxHold', foreign_key: :user_id, primary_key: :user_id, optional: true
+      belongs_to :box_proxy_buy, ->(o) { where(box_specification_id: o.good.box_specification_id, price: o.single_price) }, foreign_key: :organ_id, primary_key: :organ_id, optional: true
 
       after_save_commit :init_box_hold, if: -> { good.respond_to?(:box_specification_id) && saved_change_to_status? && ['paid'].include?(status) }
+      after_save_commit :init_box_proxy_buy, if: -> { good.respond_to?(:box_specification_id) && saved_change_to_status? && ['paid'].include?(status) }
     end
 
     def init_box_hold
       box_hold || build_box_hold
       box_hold.save
+    end
+
+    def init_box_proxy_sell
+      box_proxy_buy || build_box_proxy_buy
+      box_proxy_buy.save
     end
 
   end
