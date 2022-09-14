527c5e1d1e53f91d90c536ca37d9b2a510f37fc9
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-14 11:12:09 +0800

    idnex tbody

--- app/controllers/ship/admin/box_hosts_controller.rb
+++ app/controllers/ship/admin/box_hosts_controller.rb
@@ -3,6 +3,16 @@ module Ship
 
     def index
       @box_hosts = BoxHost.includes(:box_specification).page(params[:page])
+
+      @box_specifications = BoxSpecification.where.not(id: @box_hosts.pluck(:box_specification_id).uniq)
+    end
+
+    private
+    def box_host_params
+      p = params.fetch(:box_host, {}).permit(
+        :box_specification_id
+      )
+      p.merge! default_form_params
     end
 
   end
