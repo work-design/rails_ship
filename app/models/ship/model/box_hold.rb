bfb3a767493c1327517d60f74beb69a47b14d02f
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-15 17:29:25 +0800

    items lists

--- app/models/ship/model/box_hold.rb
+++ app/models/ship/model/box_hold.rb
@@ -19,10 +19,11 @@ module Ship
       has_many :box_sells, ->(o) { where(organ_id: o.organ_id, user_id: o.user_id, member_id: o.member_id) }, primary_key: :box_specification_id, foreign_key: :box_specification_id
       has_many :box_buys, ->(o) { where(organ_id: o.organ_id, user_id: o.user_id, member_id: o.member_id) }, primary_key: :box_specification_id, foreign_key: :box_specification_id
       has_many :boxes, ->(o) { where(organ_id: o.organ_id, owned_user_id: o.user_id) }, primary_key: :box_specification_id, foreign_key: :box_specification_id
+      has_many :items, ->(o) { where(member_id: o.member_id, good_type: 'Ship::BoxHost', good_id: o.box_host.id) }, class_name: 'Trade::Item', primary_key: :user_id, foreign_key: :user_id
     end
 
     def average_price
-      box_host.items.where(user_id: user_id, member_id: member_id).average(:single_price)&.to_fs(:rounded, precision: 3)
+      items.average(:single_price)&.to_fs(:rounded, precision: 3)
     end
 
   end
