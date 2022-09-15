9f2ad441ec845a7acf09728cfea49e1e07ace4fd
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-15 17:12:16 +0800

    rents list details

--- app/controllers/ship/admin/rents_controller.rb
+++ app/controllers/ship/admin/rents_controller.rb
@@ -1,3 +1,14 @@
-class Ship::Admin::RentsController < Ship::Admin::BaseController
+module Ship
+  class Admin::RentsController < Admin::BaseController
+    before_action :set_box
 
+    def index
+      @rents = @box.rents.order(id: :desc).page(params[:page])
+    end
+
+    private
+    def set_box
+      @box = Box.find params[:box_id]
+    end
+  end
 end
