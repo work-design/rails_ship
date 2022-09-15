0e432558f784efa12a1e48e92a820839db6c835f
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-15 16:21:01 +0800

    find by boxeid

--- app/controllers/ship/boxes_controller.rb
+++ app/controllers/ship/boxes_controller.rb
@@ -12,11 +12,11 @@ module Ship
 
     def qrcode
       if current_user.organ_ids.include?(@box.organ_id)
-        redirect_to({ controller: 'ship/me/boxes', action: 'qrcode', id: params[:id], host: @box.organ.host }, allow_other_host: true)
+        redirect_to({ controller: 'ship/me/boxes', action: 'qrcode', id: @box.id, host: @box.organ.host }, allow_other_host: true)
       elsif current_user.organ_ids.present?
-        redirect_to({ controller: 'ship/boxes', action: 'in_edit', id: params[:id] }, allow_other_host: true)
+        redirect_to({ controller: 'ship/boxes', action: 'in_edit', id: @box.id }, allow_other_host: true)
       else
-        redirect_to({ controller: 'ship/my/boxes', action: 'qrcode', id: params[:id] }, allow_other_host: true)
+        redirect_to({ controller: 'ship/my/boxes', action: 'qrcode', id: @box.id }, allow_other_host: true)
       end
     end
 
@@ -41,7 +41,7 @@ module Ship
     end
 
     def set_box
-      @box = Box.find params[:id]
+      @box = Box.find_by code: params[:id]
     end
 
     def set_new_order
