af5e377496ee0d75d1b149de415da253413ca2d8
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-17 11:47:30 +0800

    one can edit rents

--- app/controllers/ship/admin/rents_controller.rb
+++ app/controllers/ship/admin/rents_controller.rb
@@ -1,6 +1,7 @@
 module Ship
   class Admin::RentsController < Admin::BaseController
     before_action :set_box
+    before_action :set_rent, only: [:show, :edit, :update, :destroy, :actions]
 
     def index
       @rents = @box.rents.order(id: :desc).page(params[:page])
@@ -9,6 +10,18 @@ module Ship
     private
     def set_box
       @box = Box.find params[:box_id]
+    end
+
+    def set_rent
+      @rent = @box.rents.find params[:id]
+    end
+
+    def rent_params
+      params.fetch(:rent, {}).permit(
+        :start_at,
+        :finish_at,
+        :estimate_finish_at
+      )
     end
   end
 end
