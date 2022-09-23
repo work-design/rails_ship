09231b498eb923dbaf8709495ad0b73cfa62ab9b
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-23 11:48:16 +0800

    show item order

--- app/controllers/ship/my/items_controller.rb
+++ app/controllers/ship/my/items_controller.rb
@@ -4,11 +4,11 @@ module Ship
     before_action :set_item, only: [:show, :actions]
 
     def index
-      @items = @box_hold.items.aim_use.page(params[:page])
+      @items = @box_hold.items.includes(:order).aim_use.page(params[:page])
     end
 
     def rent
-      @items = @box_hold.items.aim_rent.deliverable.page(params[:page])
+      @items = @box_hold.items.includes(:order).aim_rent.deliverable.page(params[:page])
     end
 
     private
