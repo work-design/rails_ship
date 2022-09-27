aca536de6f25e0b4fc26557626772ada42f9fb9b
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-27 15:56:45 +0800

    test with compute proxy number

--- app/models/ship/ext/item.rb
+++ app/models/ship/ext/item.rb
@@ -9,6 +9,8 @@ module Ship
       belongs_to :box_hold, ->(o) { where(organ_id: o.organ_id, member_id: o.member_id, box_specification_id: o.good.box_specification_id) }, class_name: 'Ship::BoxHold', foreign_key: :user_id, primary_key: :user_id, optional: true
       belongs_to :box_proxy_buy, ->(o) { where(price: o.single_price) }, class_name: 'Ship::BoxProxyBuy', foreign_key: :good_id, primary_key: :box_host_id, optional: true
 
+      after_save :reset_proxy_count, if: -> { saved_change_to_rest_number? }
+      after_destroy :reset_proxy_count
       after_save_commit :init_box_hold, if: -> { good.respond_to?(:box_specification_id) && saved_change_to_status? && ['deliverable'].include?(status) }
       after_save_commit :init_box_proxy_buy, if: -> { good_type == 'Ship::BoxHost' && saved_change_to_status? && ['deliverable'].include?(status) }
     end
@@ -21,6 +23,10 @@ module Ship
     def init_box_proxy_buy
       box_proxy_buy || build_box_proxy_buy
       box_proxy_buy.save
+    end
+
+    def reset_proxy_count
+      box_proxy_buy&.reset_buyable_count!
     end
 
   end
